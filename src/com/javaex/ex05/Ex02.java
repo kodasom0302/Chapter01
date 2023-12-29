38e9473b1a8422862d869b536e0acc68db104497
dasom <dasom589@gmail.com>
2023-12-29 11:32:32 +0900

    2023-12-29

---
+++ src/com/javaex/ex05/Ex02.java
@@ -0,0 +1,24 @@
+package com.javaex.ex05;
+
+public class Ex02 {
+
+	public static void main(String[] args) {
+		
+		for (int i=1; i<=6; i++) {
+			i=(int)(Math.random()*45)+1;
+		}
+		
+		/*
+		int n1=(int)(Math.random()*45)+1;
+		int n2=(int)(Math.random()*45)+1;
+		int n3=(int)(Math.random()*45)+1;
+		int n4=(int)(Math.random()*45)+1;
+		int n5=(int)(Math.random()*45)+1;
+		int n6=(int)(Math.random()*45)+1;
+		
+		System.out.println(n1+" "+n2+" "+n3+" "+n4+" "+n5+" "+n6);
+		*/
+
+	}
+
+}
